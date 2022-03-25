692066ad2f460d4617830232ef9d93751f3e2b49
pragnakasthruri <kpragna25@gmail.com>
2022-03-25 17:28:33 +0530

    Adding the code to throw custom exception when file type is incorrect

--- src/test/java/com/bridgelabz/IndianCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/IndianCensusAnalyserTest.java
@@ -3,9 +3,12 @@ package com.bridgelabz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.PrimitiveIterator;
+
 public class IndianCensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_TYPE = "./src/test/resources/IndiaStateCensusData.json";
 
 
     @Test
@@ -24,6 +27,16 @@ public class IndianCensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+
+    @Test
+    public void givenIndianCensusData_WithWrongTYPE_ShouldThrowException() {
+        try {
+            IndianStatesCensusAnalyser censusAnalyser = new IndianStatesCensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_TYPE);
+        } catch (CensusAnalyserException e) {
+            Assertions.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_TYPE_PROBLEM, e.type);
         }
     }
 }
