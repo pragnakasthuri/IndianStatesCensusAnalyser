36d61f7287a412853ed7343e3ccae059d0a8110e
pragnakasthruri <kpragna25@gmail.com>
2022-03-25 13:51:13 +0530

    Adding the code for IndianStatesCensusAnalyser displaying welcom message

---
+++ src/com/bridgelabz/IndianStatesCensusAnalyser.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+/**
+ * Creating IndianStatesCensusAnalyser class to display the welcome message
+ */
+public class IndianStatesCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Indian States Census Analyser");
+    }
+}
